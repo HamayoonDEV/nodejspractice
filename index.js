8397e07a820f837a850c3ee62fe52c4481eae59a
unknown <hamayoon.47611@iqra.edu.pk>
2023-04-05 16:48:53 +0500

    nodejs

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import userRoutes from "./routes/user.js";
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.use("/user", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello from home page");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is runing on http://localhost:${PORT}`);
+});
